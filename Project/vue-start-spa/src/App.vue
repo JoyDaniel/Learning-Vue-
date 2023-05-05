2ca84849c7210470e68f8eb749feb4d7c8a1638e
JoyDaniel <joydanieljoey@gmail.com>
2023-05-05 17:14:04 +0530

    Validating forms

--- Project/vue-start-spa/src/App.vue
+++ Project/vue-start-spa/src/App.vue
@@ -12,18 +12,23 @@
    <page-viewer 
    v-if="pages.length > 0"
    :page="pages[activepage]"></page-viewer>
+
+   <create-page
+        :page-created="pageCreated"
+   ></create-page>
 </template>
 
 <script>
 
 import Navbar from './components/Navbar.vue';
 import PageViewer from './components/PageViewer.vue';
-
+import CreatePage from './components/CreatePage.vue';
 
 export default {
     components:{
         PageViewer, 
-        Navbar
+        Navbar,
+        CreatePage
     },
     created(){
         this.getPages();
@@ -41,6 +46,9 @@ export default {
 
             this.pages = data;
 
+        },
+        pageCreated(pageObj){
+            console.log(pageObj);
         }
     }
 }
---
+++ Project/vue-start-spa/src/components/CreatePage.vue
@@ -0,0 +1,101 @@
+<template>
+    <form action="" class="container mb-3">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Page Title
+                    </label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="pageTitle"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Content
+                    </label>
+                    <textarea
+                        type="text"
+                        class="form-control"
+                        rows="5"
+                        v-model="content"
+                    ></textarea>
+                </div>
+            </div>
+            <div class="col">
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Link Text
+                    </label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="linkText"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Link URL
+                    </label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="linkUrl"
+                    />
+                </div>
+                <div class="row mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox">
+                        <label class="form-check-label" for="gridChceck1">
+                        Published
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-5">
+                <button
+                    class="btn btn-primary"
+                    :disabled="isFormInvalid"
+                    @click.prevent="submitForm()"
+                > Create Page </button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default{
+        props:['pageCreated'],
+        computed:{
+             isFormInvalid(){
+                return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl
+             }
+        },
+        data(){
+            return{
+                pageTitle:'',
+                content:'',
+                linkText:'',
+                linkUrl:''
+            }
+        },
+        methods:{
+            submitForm(e){
+                if(!this.pageTitle || !this.content || !this.linkText || !this.linkUrl){
+                    alert("Please fill all the details..");
+                    return;
+                }
+                this.pageCreated({
+                    pageTitle: this.pageTitle,
+                    content: this.content,
+                    link:{
+                        text: this.linkText,
+                        url: this.linkUrl
+                    }
+                })
+            }
+        }
+    }
+</script>
